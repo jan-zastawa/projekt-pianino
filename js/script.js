7fd6bdc6d466326aa4a5d570e1f738d458f25808
Jan Zastawa <jan.zastawa@gmail.com>
2021-04-21 13:46:32 +0200

    zmiany z 21.04.2021

--- js/script.js
+++ js/script.js
@@ -21,12 +21,82 @@ document.addEventListener('keydown', e => {
     switch(e.code.substr(3,1).toLowerCase()){
         case "a":
             document.getElementById("C").play();
+            console.log(document.querySelector("div[note=C]").classList.add("white-active"));
             break;
         case "w":
             document.getElementById("Cs").play();
             break;
         case "s":
             document.getElementById("D").play();
+            break;
+        case "e":
+            document.getElementById("Ds").play();
+            break;
+        case "d":
+            document.getElementById("E").play();
+                break;
+        case "f":
+            document.getElementById("F").play();
+            break;
+        case "t":
+            document.getElementById("Fs").play();
+            break;
+        case "g":
+            document.getElementById("G").play();
+            break;
+        case "y":
+            document.getElementById("Gs").play();
+            break;
+        case "h":
+            document.getElementById("A").play();
+            break;
+        case "u":
+            document.getElementById("As").play();
+            break;
+        case "j":
+            document.getElementById("B").play();
+            break;
+
+    }
+})
+
+document.addEventListener('keyup', e => {
+    switch(e.code.substr(3,1).toLowerCase()){
+        case "a":
+            document.getElementById("C").play();
+            break;
+        case "w":
+            document.getElementById("Cs").play();
+            break;
+        case "s":
+            document.getElementById("D").play();
+            break;
+        case "e":
+            document.getElementById("Ds").play();
+            break;
+        case "d":
+            document.getElementById("E").play();
+                break;
+        case "f":
+            document.getElementById("F").play();
+            break;
+        case "t":
+            document.getElementById("Fs").play();
+            break;
+        case "g":
+            document.getElementById("G").play();
+            break;
+        case "y":
+            document.getElementById("Gs").play();
+            break;
+        case "h":
+            document.getElementById("A").play();
+            break;
+        case "u":
+            document.getElementById("As").play();
+            break;
+        case "j":
+            document.getElementById("B").play();
             break;
 
     }
